35522777738dd10c4f0ec8e5747076817cd6d6c3
emmerritt <emmerritt@mac.com>
2022-07-21 00:07:16 -0700

    Created db adapters for carts table

--- db/carts.js
+++ db/carts.js
@@ -14,6 +14,66 @@ const createCartItem = async ({userId, inventoryId, count}) => {
     }
 }
 
+const getCartItemsByUserId = async (userId) => {
+    try {
+        const { rows: items } = await client.query(`
+          SELECT carts.count, inventories.id AS "inventoryId", products.id AS "productId", products.name, products.price, products.image, brands.name AS "brand", sizes.gender, sizes.size  FROM carts
+          JOIN inventories
+          ON carts."inventoryId"=inventories.id
+          JOIN products
+          ON inventories."productId"=products.id
+          JOIN brands
+          ON products."brandId"=brands.id
+          JOIN sizes
+          ON inventories."sizeId"=sizes.id
+          WHERE carts."userId"=$1;
+        `, [userId]);
+
+        const cart = {
+            userId,
+            cart: items
+        }
+
+        return cart;
+      } catch (error) {
+        throw error;
+      }
+}
+
+const updateCartItemCount = async ({userId, inventoryId, count}) => {
+    try {
+        await client.query(`
+            UPDATE carts
+            SET count=$1
+            WHERE carts."userId"=$2 AND carts."inventoryId"=$3;
+        `, [count, userId, inventoryId]);
+
+        const updatedCart = await getCartItemsByUserId(userId);
+
+        return updatedCart;
+      } catch (error) {
+        throw error;
+      }
+}
+
+const removeCartItem = async ({userId, inventoryId}) => {
+    try {
+        await client.query(`
+            DELETE FROM carts
+            WHERE carts."userId"=$1 AND carts."inventoryId"=$2;
+        `, [userId, inventoryId]);
+
+        const updatedCart = await getCartItemsByUserId(userId);
+
+        return updatedCart;
+      } catch (error) {
+        throw error;
+      }
+}
+
 export { 
-    createCartItem
+    createCartItem,
+    getCartItemsByUserId,
+    updateCartItemCount,
+    removeCartItem
  }
