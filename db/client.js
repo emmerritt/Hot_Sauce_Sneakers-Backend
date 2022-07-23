687fc4a273761cac30dabe24c10dbf647d63877c
Jacob Woehrle <99307925+Cakej7@users.noreply.github.com>
2022-07-23 11:18:46 -0700

    Revert "Develop"

--- db/client.js
+++
@@ -1,8 +0,0 @@
-import pkg from 'pg';
-const { Client } = pkg;
-
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/hot-sauce-sneakers-db';
-
-const client = new Client(connectionString);
-
-export { client };
